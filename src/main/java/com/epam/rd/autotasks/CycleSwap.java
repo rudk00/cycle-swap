1db94b2638e05e551ef142de3e7cdff33d1aa140
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-04 14:56:28 +0300

    Cycle swap

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,11 +1,31 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 class CycleSwap {
-    static void cycleSwap(int[] array) {
-
+    public static void main(String[] args) {
+        int[] array = new int[]{ 1, 3, 2, 7, 4 };
+        CycleSwap.cycleSwap(array);
+        System.out.println(Arrays.toString(array));
     }
-
-    static void cycleSwap(int[] array, int shift) {
-
+    public static void cycleSwap(int[] array) {
+        int[] ar = new int[array.length];
+        if (array.length == 0) {
+        } else {
+            System.arraycopy(array, 0, ar, 0, ar.length);
+            System.arraycopy(array, 0, ar, 1, ar.length - 1);
+            System.arraycopy(array, array.length - 1, ar, 0, 1);
+            System.arraycopy(ar, 0, array, 0, ar.length);
+        }
+    }
+    public static void cycleSwap(int[] array, int shift) {
+        int[] ar = new int[array.length];
+        if (array.length == 0) {
+        } else {
+            System.arraycopy(array, 0, ar, 0, ar.length);
+            System.arraycopy(array, ar.length - shift, ar, 0, shift);
+            System.arraycopy(array, 0, ar, shift, ar.length - shift);
+            System.arraycopy(ar, 0, array, 0, ar.length);
+        }
     }
 }
